375101c528671ec43de945f4a47b5914755a7833
dunn0052 <dunn0052@gmail.com>
2017-09-13 21:11:56 -0500

    Add files via upload

---
+++ PAL-grouping.py
@@ -0,0 +1,31 @@
+#PAL group randomizer
+
+import random
+
+#n is number of people in class and m is number per each group
+
+def randomizer(n, m):
+    a = []
+    for i in range(n):
+        a.append(i)
+        
+    random.shuffle(a)
+
+   # for i in range(len(a)):
+    #    print()
+    
+    k = 0
+    j = n%m
+    while(k < n/m):
+        print("group", k + 1)
+        for i in range(m):
+            print(a[m*i + k] + 1)
+            if(j != 0 and i == m - 1):
+                print(a[n - j] + 1)
+                j = j - 1
+        k = k + 1
+        print("")
+
+
+randomizer(18,4)
+
