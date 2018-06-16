697ceef4844cabb38d1bed943903d8dc32b6b475
dunn0052 <dunn0052@gmail.com>
2018-06-16 15:00:25 -0500

    Delete 12_test.py

--- Dice Functions/12_test.py
+++
@@ -1,9 +0,0 @@
-# convoluted dice percentage test
-
-from dice_sum import *
-
-sixdsix = sum_dice()
-sixdsix.series = sixdsix.make_die()
-sixdsix.mult(sixdsix.make_die(), 5)
-sixdsix.chance()
-sixdsix.expected_output()
