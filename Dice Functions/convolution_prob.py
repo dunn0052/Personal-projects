9571187e1f9565f654e0174c0ec00334369d0ae1
Kevin Dunn <dunn0052@gmail.com>
2018-09-05 15:43:16 -0500

    convolutions with integer combinations

--- Dice Functions/convolution_prob.py
+++ Dice Functions/convolution_prob.py
@@ -3,10 +3,12 @@
 #n = number of dice, k = sum total, f = number of die faces
 def conv(n, k, f):
     s = 0
-    if n == 1:
-        return 1/f
-    if n == k:
-        return 1/(f**n)
+    #safety measures
+    if k < n:
+        print("Cannot produce", k,"with", n,"dice")
+        return s
+    if n == 1 or n == k:
+        return 1#/f
     for j in range(max(1,k-f*(n-1)), min(f+1, k-(n-1)+1)):
         s += conv(n-1, k-j, f) * conv(1, j, f)
     return s
